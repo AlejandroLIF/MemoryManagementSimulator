048febc228dd0d7e630fceb275c0bd90dca280dd
BlackRage83 <BlackRage83@github.com>
2015-05-11 21:40:14 -0500

    Modified main to accept input from the basic input file.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <iterator>
 
@@ -12,12 +13,12 @@ using namespace std;
 int main(int argc, char* argv[]){
     string line;
     if(argc != 2){
-        printf("Usage: %s, input.txt", argv[0]);
+        printf("Usage: %s, input.txt\n", argv[0]);
         return -1;
     }
     ifstream inputFile(argv[1]);
     if(!inputFile.is_open()){
-        printf("Unable to open file \"%s\"!", argv[1]);
+        printf("Unable to open file \"%s\"!\n", argv[1]);
         return -1;
     }
     
@@ -29,25 +30,30 @@ int main(int argc, char* argv[]){
         switch(temp){
             case 'P':
             case 'p':
+                printf("P\t%i\t%i\n", atoi(tokens.at(1).c_str()), atoi(tokens.at(2).c_str()));
                 break;
             case 'A':
             case 'a':
+                printf("A\t%i\t%i\t%i\n", atoi(tokens.at(1).c_str()), atoi(tokens.at(2).c_str()), atoi(tokens.at(3).c_str()));
                 break;
             case 'L':
             case 'l':
+                printf("L\t%i\n", atoi(tokens.at(1).c_str()));
                 break;
             case 'F':
             case 'f':
+                printf("F\n");
                 break;
             case 'E':
             case 'e':
+                printf("E\n");
                 break;
             default:
                 //Should never happen
-                printf("Unknown command \"%c\". Exiting.", temp);
+                printf("Unknown command \"%c\".\n\tExiting...\n", temp);
                 return -1;
                 break;
         }
     }
-    
+    return 0;   
 }
